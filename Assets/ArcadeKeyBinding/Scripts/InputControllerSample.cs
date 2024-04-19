ea2bb48206395b971c0bc99da38bb13884acf2cc
Mikel Matticoli <mikel.matticoli@gmail.com>
2024-04-18 21:41:04 -0400

    Cleanup

--- Assets/ArcadeKeyBinding/Scripts/InputControllerSample.cs
+++ Assets/ArcadeKeyBinding/Scripts/InputControllerSample.cs
@@ -3,30 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
-public class ArcadeControlTest : MonoBehaviour
+
+public class InputControllerSample : MonoBehaviour
 {
-    // B11,B3,B4,B1
-    public GameObject B11;
-    public GameObject B3;
-    public GameObject B4;
-    public GameObject B1;
-    public ArcadeControl arcadeControl;
+    // Home,J,K,L
+    public GameObject Home, J, K, L; // Indicators
+    public ArcadeControl arcadeControl; // ArcadeControl class generated from controller mapping
     public GameObject joystick;
     public GameObject trackball;
     public TMPro.TextMeshProUGUI debug;
 
     private void Start()
     {
-
         // enable control
         arcadeControl = new ArcadeControl();
         arcadeControl.Enable();
 
         // subscribe to the button click action
-        arcadeControl.Core.B11.performed += OnButtonClick;
-        arcadeControl.Core.B3.performed += OnButtonClick;
-        arcadeControl.Core.B4.performed += OnButtonClick;
-        arcadeControl.Core.B1.performed += OnButtonClick;
+        arcadeControl.Core.Home.performed += OnButtonClick;
+        arcadeControl.Core.J.performed += OnButtonClick;
+        arcadeControl.Core.K.performed += OnButtonClick;
+        arcadeControl.Core.L.performed += OnButtonClick;
         arcadeControl.Core.JoystickX.performed += OnJoystickTriggered;
         arcadeControl.Core.JoystickY.performed += OnJoystickTriggered;
         arcadeControl.Core.Trackball.performed += OnTrackballTriggered;
@@ -39,32 +36,30 @@ public class ArcadeControlTest : MonoBehaviour
         debug.text = (context.ToString());
         switch (buttonTriggered)
         {
-            case "B11":
-                B11.GetComponent<Animator>().Play("ButtonAnimation", 0);
+            case "Home":
+                Home.GetComponent<Animator>().Play("ButtonAnimation", 0);
                 break;
-            case "B3":
-                B3.GetComponent<Animator>().Play("ButtonAnimation", 0);
+            case "J":
+                J.GetComponent<Animator>().Play("ButtonAnimation", 0);
                 break;
-            case "B4":
-                B4.GetComponent<Animator>().Play("ButtonAnimation", 0);
+            case "K":
+                K.GetComponent<Animator>().Play("ButtonAnimation", 0);
                 break;
-            case "B1":
-                B1.GetComponent<Animator>().Play("ButtonAnimation", 0);
+            case "L":
+                L.GetComponent<Animator>().Play("ButtonAnimation", 0);
                 break;
         }
-
     }
 
     void OnJoystickTriggered(InputAction.CallbackContext context)
     {
         joystick.GetComponentInChildren<Animator>().Play("JoystickInteraction", 0);
         debug.text = (context.ToString());
-
     }
+    
     void OnTrackballTriggered(InputAction.CallbackContext context)
     {
         trackball.GetComponentInChildren<Animator>().Play("TrackballInteraction", 0);
         debug.text = (context.ToString());
-
     }
 }
